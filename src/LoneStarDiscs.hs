524eed379b0b96b3e7be0b8c10d7c4fcbd01bb42
Chapman Shoop <chapman.shoop@gmail.com>
2024-08-20 22:32:03 -0500

    Add more DistanceDrivers.

--- src/LoneStarDiscs.hs
+++ src/LoneStarDiscs.hs
@@ -25,5 +25,11 @@ loneStarDiscs =
   [ Disc {mold = "Warbird", discType = DistanceDriver, speed = 12, glide = 6, turn = (-1), fade = 3},
     Disc {mold = "Growler", discType = DistanceDriver, speed = 12, glide = 6, turn = (-3), fade = 2},
     Disc {mold = "Bayonet", discType = DistanceDriver, speed = 13, glide = 5, turn = (-2), fade = 2},
-    Disc {mold = "Bowie",   discType = DistanceDriver, speed = 13, glide = 5, turn = (-1), fade = 3}
+    Disc {mold = "Seguin", discType = DistanceDriver, speed = 13, glide = 5, turn = 0, fade = 3},
+    Disc {mold = "Tombstone", discType = DistanceDriver, speed = 13, glide = 4, turn = 0, fade = 4},
+    Disc {mold = "Harrier", discType = DistanceDriver, speed = 12, glide = 6, turn = (-4), fade = 1},
+    Disc {mold = "Nimitz", discType = DistanceDriver, speed = 11, glide = 5, turn = (-1), fade = 3},
+    Disc {mold = "Curl", discType = DistanceDriver, speed = 11, glide = 5, turn = (-1), fade = 2},
+    Disc {mold = "Tumbleweed", discType = DistanceDriver, speed = 10, glide = 6, turn = (-3), fade = 1},
+    Disc {mold = "Bowie", discType = DistanceDriver, speed = 13, glide = 5, turn = (-1), fade = 3}
   ]
